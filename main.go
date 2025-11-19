e0722f87da8737f5d902e9ac1d46a0a9ccd7dcb5
Andreas Kaempf <andreas.kaempf@soundience.com>
2025-11-19 13:40:49 +0100

    Fixed edit_project; style fixes

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func main() {
 	r.GET("/", showProjects)
 	r.GET("/projects", showProjects)
 	r.GET("/project/:id", showProject)
-	r.GET("/edit_project", editProject)
+	r.GET("/edit_project/:id", editProject)
 	r.POST("/save_project", saveProjectForm)
 	r.GET("/delete_project/:id", deleteProjectHandler)
 
